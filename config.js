e70d5655b6bee8a0cf3c1f773100bac5a45650e7
Benjamin Becker <benjamin.becker@teamnorthwoods.com>
2023-05-27 22:11:09 -0400

    Configure renovate.

--- config.js
+++ config.js
@@ -2,15 +2,8 @@ module.export = {
     extends: ['config:base'],
     timezone: 'America/New York',
     schedule: ['after 6am and before 9am'],
-    rebaseWhen: 'conflicted',
-    updateLockFiles: true,
-    prCreation: 'immediate',
-    rangeStrategy: 'auto',
     prHourlyLimit: 5,
     prConcurrentLimit: 5,
-    requiredStatusChecks: null, // bors will always be pending and this feature is only partially implemented so setting to not wait on pr status
-    automergeType: 'pr-comment',
-    automergeComment: 'bors r+',
     labels: ['chore', 'renovate 🎨'],
     npm: {
       fileMatch: ['(^|/)package.json$', '(^|/)package.dist.json$']
